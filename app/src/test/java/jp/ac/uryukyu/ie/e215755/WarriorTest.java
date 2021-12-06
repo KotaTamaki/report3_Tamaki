c1c3d7b918e110ae91c474b49acbafe47765186f
KotaTamaki <e215755@ie.u-ryukyu.ac.jp>
2021-12-07 01:21:00 +0900

    7th

--- app/src/test/java/jp/ac/uryukyu/ie/e215755/WarriorTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e215755/WarriorTest.java
@@ -9,16 +9,18 @@ public class WarriorTest {
     @Test
     void attachWithWeponSkillTest() {
         int defaultEnemyHP = 100;
-        Warrior warrior = new Warrior("Warrior", 10, 100);
-        Enemy enemy = new Enemy("スライム", defaultEnemyHP, 100);
-        int weponSkillDamage = (int)(warrior.attack * 1.5);
-        int enemyDamage = 0;
+        int defaultWorriorHP = 100;
+        int defaultWarriorAttack = 10;
+        int defaultEnemyAttack = 15;
+        Warrior warrior = new Warrior("勇者", defaultWorriorHP, defaultWarriorAttack);
+        Enemy enemy = new Enemy("スライム", defaultEnemyHP, defaultEnemyAttack);
+        
 
-        for(int i=0; i<5; i++){
+        for(int i=0; i<3; i++){
+            int Hp=enemy.getHitPoint();
             warrior.attackWithWeponSkill(enemy);
-            enemyDamage = defaultEnemyHP - enemy.hitPoint;
-            assertEquals(weponSkillDamage, enemyDamage);
-            defaultEnemyHP -= enemyDamage;
+            enemy.attack(warrior);
+            assertEquals((int)(defaultWarriorAttack * 1.5), Hp - enemy.getHitPoint());
         }
     }
 }
